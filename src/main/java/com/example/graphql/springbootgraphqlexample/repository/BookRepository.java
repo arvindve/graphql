6358ea2b87feefb000066c34a020838ca3072a73
arvindve <arvindcse.verma@gmail.com>
2018-03-21 08:14:17 +0800

    GraphQL example

---
+++ src/main/java/com/example/graphql/springbootgraphqlexample/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.example.graphql.springbootgraphqlexample.repository;
+
+import com.example.graphql.springbootgraphqlexample.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, String> {
+}
